1d2de61b31215146bc9799150da4bf0717d395c6
JaeJoon Jung <rgbi3307@nate.com>
2020-09-16 18:01:29 +0900

    D20200916 Studing in load_balance()...
    Signed-off-by: JaeJoon Jung <rgbi3307@nate.com>

--- main.c
+++ main.c
@@ -11,6 +11,8 @@ DECLARE_PER_CPU_SHARED_ALIGNED(struct rq, runqueues);
 #define task_rq(p)              cpu_rq(task_cpu(p))
 #define cpu_curr(cpu)           (cpu_rq(cpu)->curr)
 #define raw_rq()                raw_cpu_ptr(&runqueues)
+
+Studing in load_balance()...
 */
 
 int main(void)
