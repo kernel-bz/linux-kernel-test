da0d31ac20668008d08ac2a851636f35bc54d356
Jung-JaeJoon <rgbi3307@naver.com>
2021-02-04 15:22:28 +0900

    D20210204 Cancel pthred_mutex_lock. (1) include/linux/spinlock.h (2) test/drivers/dtb-test.c 	Bug--> dtb_test_read_file(): realloc()
    Signed-off-by: Jung-JaeJoon <rgbi3307@naver.com>

--- include/linux/spinlock.h
+++ include/linux/spinlock.h
@@ -10,27 +10,33 @@
 #define __SPIN_LOCK_UNLOCKED(x)	(pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER
 #define spin_lock_init(x)	pthread_mutex_init(x, NULL)
 
-#define spin_lock(x)			pthread_mutex_lock(x)
-#define spin_unlock(x)			pthread_mutex_unlock(x)
-#define spin_lock_bh(x)			pthread_mutex_lock(x)
-#define spin_unlock_bh(x)		pthread_mutex_unlock(x)
-#define spin_lock_irq(x)		pthread_mutex_lock(x)
-#define spin_unlock_irq(x)		pthread_mutex_unlock(x)
-#define spin_lock_irqsave(x, f)			(void)f, pthread_mutex_lock(x)
-#define spin_unlock_irqrestore(x, f)	(void)f, pthread_mutex_unlock(x)
+//#define spin_lock(x)			pthread_mutex_lock(x)
+#define spin_lock(x)
+//#define spin_unlock(x)			pthread_mutex_unlock(x)
+#define spin_unlock(x)
+
+#define spin_lock_bh(x)
+#define spin_unlock_bh(x)
+#define spin_lock_irq(x)
+#define spin_unlock_irq(x)
+//#define spin_lock_irqsave(x, f)		(void)f, pthread_mutex_lock(x)
+#define spin_lock_irqsave(x, f)
+//#define spin_unlock_irqrestore(x, f)	(void)f, pthread_mutex_unlock(x)
+#define spin_unlock_irqrestore(x, f)
 
 #define raw_spin_lock_init(lock)	do { } while(0)
 
-#define raw_spin_lock(x)			pthread_mutex_lock(x)
-#define raw_spin_unlock(x)			pthread_mutex_unlock(x)
-#define raw_spin_lock_irq(x)		pthread_mutex_lock(x)
-#define raw_spin_unlock_irq(x)		pthread_mutex_unlock(x)
-//#define raw_spin_lock_irqsave(x, f)			(void)f, pthread_mutex_lock(x)
+#define raw_spin_lock(x)
+#define raw_spin_unlock(x)
+#define raw_spin_lock_irq(x)
+#define raw_spin_unlock_irq(x)
+//#define raw_spin_lock_irqsave(x, f)		(void)f, pthread_mutex_lock(x)
 #define raw_spin_lock_irqsave(x, f)
 //#define raw_spin_unlock_irqrestore(x, f)	(void)f, pthread_mutex_unlock(x)
 #define raw_spin_unlock_irqrestore(x, f)
 
-#define raw_spin_trylock(x)			raw_spin_lock(x)
+//#define raw_spin_trylock(x)	raw_spin_lock(x)
+#define raw_spin_trylock(x)		1
 
 # define raw_spin_lock_nested(lock, subclass)		\
     raw_spin_lock(((void)(subclass), (lock)))
@@ -44,12 +50,12 @@
 
 static inline void arch_spin_lock(arch_spinlock_t *mutex)
 {
-	pthread_mutex_lock(mutex);
+    //pthread_mutex_lock(mutex);
 }
 
 static inline void arch_spin_unlock(arch_spinlock_t *mutex)
 {
-	pthread_mutex_unlock(mutex);
+    //pthread_mutex_unlock(mutex);
 }
 
 static inline bool arch_spin_is_locked(arch_spinlock_t *mutex)
